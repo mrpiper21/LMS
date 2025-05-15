1ee18591f89e9eba5b93ac7098dd2bd2bd92aef3
Bernard <benbaah104@gmail.com>
2025-05-15 19:31:54 +0000

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,26 +4,28 @@ import LoginPage from "../pages/auth-pages/LoginPage";
 import AppLayout from "../layout/AppLayout";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>Hello world, H</div>
-    },
-    {
-        path: "/login",
-        element: <LoginPage />
-    },
-    {
-        path: "/home",
-        element: <AppLayout>
-            <HomePage />
-        </AppLayout>,
-        children: [
-            {
-                path: "/courses",
-                element: <div>Hello</div>
-            }
-        ]
-    }
-])
+	{
+		path: "/",
+		element: <div>Hello world, H</div>,
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/home",
+		element: (
+			<AppLayout>
+				<HomePage />
+			</AppLayout>
+		),
+		// children: [
+		//     {
+		//         path: "/courses",
+		//         element: <div>Hello</div>
+		//     }
+		// ]
+	},
+]);
 
 export default router
